75c8ac2c9752440c20dd82bf8ac5a95dcbb07ed7
rgwch <weirich@elexis.ch>
2020-05-10 17:01:15 +0200

    initial import

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+    "mystrom-wifi-button adapter settings": {
+        "en": "Adapter settings for mystrom-wifi-button",
+        "de": "Adaptereinstellungen für mystrom-wifi-button",
+        "ru": "Настройки адаптера для mystrom-wifi-button",
+        "pt": "Configurações do adaptador para mystrom-wifi-button",
+        "nl": "Adapterinstellingen voor mystrom-wifi-button",
+        "fr": "Paramètres d'adaptateur pour mystrom-wifi-button",
+        "it": "Impostazioni dell'adattatore per mystrom-wifi-button",
+        "es": "Ajustes del adaptador para mystrom-wifi-button",
+        "pl": "Ustawienia adaptera dla mystrom-wifi-button",
+        "zh-cn": "mystrom-wifi-button的适配器设置"
+    },
+    "option1": {
+        "en": "option1",
+        "de": "Option 1",
+        "ru": "Опция 1",
+        "pt": "Opção 1",
+        "nl": "Optie 1",
+        "fr": "Option 1",
+        "it": "opzione 1",
+        "es": "Opción 1",
+        "pl": "opcja 1",
+        "zh-cn": "选项1"
+    },
+    "option2": {
+        "en": "option2",
+        "de": "Option 2",
+        "ru": "option2",
+        "pt": "opção 2",
+        "nl": "Optie 2",
+        "fr": "Option 2",
+        "it": "opzione 2",
+        "es": "opcion 2",
+        "pl": "Opcja 2",
+        "zh-cn": "选项2"
+    }
+};
